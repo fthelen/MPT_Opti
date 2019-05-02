bf5727fd22c0d6b3af8e25a27eec291576e4527b
fthelen <46471530+fthelen@users.noreply.github.com>
2019-05-01 21:20:51 -0700

    less overhead

---
+++ test/third_attempt.py
@@ -0,0 +1,232 @@
+import bs4 as bs
+import datetime as dt
+import os
+import pandas as pd
+from pandas_datareader import data as pdr
+import fix_yahoo_finance as yf
+import pickle
+import requests
+import matplotlib.pyplot as plt
+from matplotlib import style
+from tqdm import tqdm
+from time import sleep
+from random import randint
+import scipy.optimize as sco
+import numpy as np
+
+# Get data
+def get_data_from_yahoo(tickers):
+    
+    yf.pdr_override()
+    table = pd.DataFrame()
+
+    for ticker in tqdm(tickers):
+        # sleep(randint(1,5)
+        df = pdr.get_data_yahoo(ticker, period=period, interval=data_interval)
+        df.rename(columns = {'Adj Close' : ticker}, inplace=True)
+        df.drop(['Open','High', 'Low', 'Close', 'Volume'], 1, inplace=True)    
+        if table.empty:
+            table = df
+        else:
+            table = table.join(df, how='outer')
+        # sleep(randint(1,5)
+    
+    table.dropna(inplace=True)
+        
+    return table     
+
+# Portfolio optimization and dislay functions
+def min_variance(mean_returns, cov_matrix):
+    num_assets = len(mean_returns)
+    args = (mean_returns, cov_matrix)
+    constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    bound = (min_weight, max_weight)
+    bounds = tuple(bound for asset in range(num_assets))
+
+    result = sco.minimize(portfolio_volatility, num_assets*[1./num_assets,], args=args,method='SLSQP', bounds=bounds, constraints=constraints)
+
+    return result
+
+def neg_sharpe_ratio(weights, mean_returns, cov_matrix, risk_free_rate):
+    p_var, p_ret = portfolio_annualized_performance(weights, mean_returns, cov_matrix)
+    return -(p_ret - risk_free_rate) / p_var
+
+def max_sharpe_ratio(mean_returns, cov_matrix, risk_free_rate):
+    num_assets = len(mean_returns)
+    args = (mean_returns, cov_matrix, risk_free_rate)
+    constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    bound = (min_weight,max_weight)
+    bounds = tuple(bound for asset in range(num_assets))
+    result = sco.minimize(neg_sharpe_ratio, num_assets*[1./num_assets,], args=args, method='SLSQP', bounds=bounds, constraints=constraints)
+    return result
+
+def portfolio_volatility(weights, mean_returns, cov_matrix):
+    return portfolio_annualized_performance(weights, mean_returns, cov_matrix)[0]
+
+def efficient_return(mean_returns, cov_matrix, target):
+    num_assets = len(mean_returns)
+    args = (mean_returns, cov_matrix)
+
+    def portfolio_return(weights):
+        return portfolio_annualized_performance(weights, mean_returns, cov_matrix)[1]
+
+    constraints = ({'type': 'eq', 'fun': lambda x: portfolio_return(x) - target},
+                   {'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    bounds = tuple((0,1) for asset in range(num_assets))
+    result = sco.minimize(portfolio_volatility, num_assets*[1./num_assets,], args=args, method='SLSQP', bounds=bounds, constraints=constraints)
+    return result
+
+def efficient_frontier(mean_returns, cov_matrix, returns_range):
+    efficients = []
+    for ret in returns_range:
+        efficients.append(efficient_return(mean_returns, cov_matrix, ret))
+    return efficients
+
+def portfolio_annualized_performance(weights, mean_returns, cov_matrix):
+    returns = np.sum(mean_returns*weights ) * annualizing_period
+    std = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(annualizing_period)
+    return std, returns
+  
+def random_portfolios(num_portfolios, mean_returns, cov_matrix, risk_free_rate):
+    num_assets = len(mean_returns)
+    results = np.zeros((3,num_portfolios))
+    weights_record = []
+    for i in tqdm(range(num_portfolios)):
+        weights = np.random.random(num_assets)
+        weights /= np.sum(weights)
+        weights_record.append(weights)
+        portfolio_std_dev, portfolio_return = portfolio_annualized_performance(weights, mean_returns, cov_matrix)
+        results[0,i] = portfolio_std_dev
+        results[1,i] = portfolio_return
+        results[2,i] = (portfolio_return - risk_free_rate) / portfolio_std_dev
+    return results, weights_record
+
+def display_simulated_ef_with_random(mean_returns, cov_matrix, num_portfolios, risk_free_rate):
+    results, weights = random_portfolios(num_portfolios,mean_returns, cov_matrix, risk_free_rate)
+    
+    max_sharpe_idx = np.argmax(results[2])
+    sdp, rp = results[0,max_sharpe_idx], results[1,max_sharpe_idx]
+    max_sharpe_allocation = pd.DataFrame(weights[max_sharpe_idx],index=table.columns,columns=['allocation'])
+    max_sharpe_allocation.allocation = [round(i*100,2)for i in max_sharpe_allocation.allocation]
+    max_sharpe_allocation = max_sharpe_allocation.T
+    
+    min_vol_idx = np.argmin(results[0])
+    sdp_min, rp_min = results[0,min_vol_idx], results[1,min_vol_idx]
+    min_vol_allocation = pd.DataFrame(weights[min_vol_idx],index=table.columns,columns=['allocation'])
+    min_vol_allocation.allocation = [round(i*100,2)for i in min_vol_allocation.allocation]
+    min_vol_allocation = min_vol_allocation.T
+    
+    print("-"*80)
+    print("Maximum Sharpe Ratio Portfolio Allocation\n")
+    print("annualized Return:", round(rp,2))
+    print("annualized Volatility:", round(sdp,2))
+    print("\n")
+    print(max_sharpe_allocation)
+    print("-"*80)
+    print("Minimum Volatility Portfolio Allocation\n")
+    print("annualized Return:", round(rp_min,2))
+    print("annualized Volatility:", round(sdp_min,2))
+    print("\n")
+    print(min_vol_allocation)
+    
+    plt.figure(figsize=(12, 9))
+    plt.scatter(results[0,:],results[1,:],c=results[2,:],cmap='viridis_r', marker='o', s=10, alpha=0.3)
+    plt.colorbar()
+    plt.scatter(sdp,rp,marker='*',color='r',s=500, label='Maximum Sharpe ratio')
+    plt.scatter(sdp_min,rp_min,marker='*',color='g',s=500, label='Minimum volatility')
+    plt.title('Simulated Portfolio Optimization based on Efficient Frontier')
+    plt.xlabel('annualized volatility')
+    plt.ylabel('annualized returns')
+    plt.legend(labelspacing=0.8)    
+
+def display_calculated_ef_with_random(mean_returns, cov_matrix, num_portfolios, risk_free_rate):
+    results, _ = random_portfolios(num_portfolios,mean_returns, cov_matrix, risk_free_rate)
+    
+    max_sharpe = max_sharpe_ratio(mean_returns, cov_matrix, risk_free_rate)
+    sdp, rp = portfolio_annualized_performance(max_sharpe['x'], mean_returns, cov_matrix)
+    max_sharpe_allocation = pd.DataFrame(max_sharpe.x,index=table.columns,columns=['allocation'])
+    max_sharpe_allocation.allocation = [round(i*100,2)for i in max_sharpe_allocation.allocation]
+    max_sharpe_allocation = max_sharpe_allocation.T
+
+    min_vol = min_variance(mean_returns, cov_matrix)
+    sdp_min, rp_min = portfolio_annualized_performance(min_vol['x'], mean_returns, cov_matrix)
+    min_vol_allocation = pd.DataFrame(min_vol.x,index=table.columns,columns=['allocation'])
+    min_vol_allocation.allocation = [round(i*100,2)for i in min_vol_allocation.allocation]
+    min_vol_allocation = min_vol_allocation.T
+    
+    print ("-"*80)
+    print ("Maximum Sharpe Ratio Portfolio Allocation\n")
+    print ("Annualised Return:", round(rp,2))
+    print ("Annualised Volatility:", round(sdp,2))
+    print ("\n")
+    print (max_sharpe_allocation)
+    print ("-"*80)
+    print ("Minimum Volatility Portfolio Allocation\n")
+    print ("Annualised Return:", round(rp_min,2))
+    print ("Annualised Volatility:", round(sdp_min,2))
+    print ("\n")
+    print (min_vol_allocation)
+    
+    plt.figure(figsize=(10, 7))
+    plt.scatter(results[0,:],results[1,:],c=results[2,:],cmap='RdYlGn', marker='o', s=10, alpha=0.3)
+    plt.colorbar()
+    plt.scatter(sdp,rp,marker='*',color='r',s=500, label='Maximum Sharpe ratio')
+    plt.scatter(sdp_min,rp_min,marker='*',color='g',s=500, label='Minimum volatility')
+
+    target = np.linspace(rp_min, 0.32, 50)
+    efficient_portfolios = efficient_frontier(mean_returns, cov_matrix, target)
+    plt.plot([p['fun'] for p in efficient_portfolios], target, linestyle='-.', color='black', label='efficient frontier')
+    plt.title('Calculated Portfolio Optimization based on Efficient Frontier')
+    plt.xlabel('annualized volatility')
+    plt.ylabel('annualized returns')
+    plt.legend(labelspacing=0.8)
+
+# date time
+date_today = dt.date.today()
+data_interval = '1wk'
+period = '5y'
+
+# Set the annualizing period for optimization calculations based on data_interval
+if data_interval == '1mo':
+    annualizing_period = 12
+elif data_interval == '1d':
+    annualizing_period = 250
+elif data_interval == '1wk':
+    annualizing_period = 52
+else: 
+    print('Please select either 1mo, 1d, or 1w for your data interval!')
+
+tickers = ['AAPL', 'STZ', 'XOM', 'ULTA', 'AMZN','COST', 'IBM', 'V', 'GS', 'HD', 'BLK', 'HON', 'NFLX', 'INTC', 'NEE', 'CGNX', 'AMBA', 'CHD']
+
+# Run data functions
+yf.pdr_override()
+table = pd.DataFrame()
+
+for ticker in tqdm(tickers):
+    df = pdr.get_data_yahoo(ticker, period=period, interval=data_interval)
+    df.rename(columns = {'Adj Close' : ticker}, inplace=True)
+    df.drop(['Open','High', 'Low', 'Close', 'Volume'], 1, inplace=True)    
+    if table.empty:
+        table = df
+    else:
+        table = table.join(df, how='outer')
+    sleep(randint(1,5))
+    
+table.dropna(inplace=True)
+            
+# Weight bounds (not sure these work yet...)
+min_weight = 0.02
+max_weight = 0.06
+
+# Variables
+returns = table.pct_change()
+mean_returns = returns.mean()
+cov_matrix = returns.cov()
+num_portfolios = 25000
+
+# Would like a way to get this automatically
+risk_free_rate = 0.0178
+
+# Run optimization and show graph
+display_calculated_ef_with_random(mean_returns, cov_matrix, num_portfolios, risk_free_rate)
+plt.show()
